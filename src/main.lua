1b368567ebd3d8af451d91ce7b7adb33a393bda3
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2025-09-27 02:07:00 +0300

    Create main.lua

---
+++ src/main.lua
@@ -0,0 +1,26 @@
+require('constants')
+
+local Player = require('Player')
+
+local player_1 = Player:new( 1, 5, 5)
+local player_2 = Player:new( 2, 50, 50)
+
+
+function love.load()
+	print('DBG:', 'load')
+	print('DBG:', 'Vestion', love.getVersion())
+
+	screen_width  = love.graphics.getWidth()
+	screen_height = love.graphics.getHeight()
+	print('DBG: size', screen_width, screen_height)
+end
+
+function love.update(dt)
+	player_1:update(dt)
+	player_2:update(dt)
+end
+
+function love.draw()
+	player_1:draw()
+	player_2:draw()
+end
