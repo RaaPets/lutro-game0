5af92a9af27b28813309c7353dac455c7991e72d
alexander <alexander.thequeen@gmail.com>
2025-09-27 21:22:25 +0300

    saving

--- src/ecs/system_draw.lua
+++ src/ecs/system_draw.lua
@@ -15,13 +15,15 @@ function System:draw(world)
 end
 
 function System.local_update(style, pos)
+	local xx = pos.x - pos.width / 2
+	local yy = pos.y - pos.height / 2
 	if style == 1 then
 		love.graphics.setColor(200, 0, 0)
 	else
 		love.graphics.setColor(0, 0, 200)
 	end
-		love.graphics.rectangle('fill', pos.x, pos.y, pos.width, pos.height)
-		love.graphics.rectangle('line', pos.x, pos.y, pos.width, pos.height)
+		love.graphics.rectangle('fill', xx, yy, pos.width, pos.height)
+		love.graphics.rectangle('line', xx, yy, pos.width, pos.height)
 end
 
 return System
